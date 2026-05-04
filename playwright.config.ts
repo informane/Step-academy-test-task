23e44cdfc820321452cb9115b562aa1a2506bb25
infro <infromane@gmail.com>
2026-05-04 12:23:51 +0300

    remove unnessery

--- playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, devices } from '@playwright/test';
-export default defineConfig({
-  projects: [
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
-});
